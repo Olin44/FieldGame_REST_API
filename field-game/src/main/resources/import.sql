5e5378584aac7159f4b1b41d6463097f0656b558
Olin44 <47002105+Olin44@users.noreply.github.com>
2020-05-18 17:27:28 +0200

    heroku deploy, test data

--- field-game/src/main/resources/import.sql
+++ field-game/src/main/resources/import.sql
@@ -1,14 +1,31 @@
-insert into map(id,name) values ('1', 'Kraków');
-insert into map(id,name) values ('2', 'Warszawa');
+insert into map(id,name) values ('1', 'Kraków - Jacek');
+insert into map(id,name) values ('2', 'Kraków - Monika');
 insert into map(id,name) values ('3', 'Kalisz');
 insert into map(id,name) values ('4', 'Wadowice');
 
-insert into object_on_map_details(id, hint, object_type, map_id) values ('3', 'hint1', 1, 1);
-insert into object_on_map_details(id, hint, object_type, map_id) values ('4', 'hint2', 0, 1);
-insert into object_on_map_details(id, hint, object_type, map_id) values ('5', 'hint3', 1, 2);
-insert into object_on_map_details(id, hint, object_type, map_id) values ('6', 'hint4', 1, 3);
-insert into object_on_map_details(id, hint, object_type, map_id) values ('7', 'hint5', 0, 3);
-insert into object_on_map_details(id, hint, object_type, map_id) values ('8', 'hint6', 1, 2);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('3', 'hint1', 1, 4);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('4', 'hint2', 0, 4);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('5', 'hint3', 1, 4);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('6', 'hint4', 1, 4);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('7', 'hint5', 0, 4);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('8', 'hint6', 1, 4);
+
+insert into object_on_map_details(id, hint, object_type, map_id) values ('9', 'Kiosk', 1, 2);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('10', 'Delikatesy', 2, 2);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('11', 'Chińczyk', 1, 2);
+
+insert into object_on_map_details(id, hint, object_type, map_id) values ('12', 'Klub Filutek', 2, 1);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('13', 'Lodowisko', 1, 1);
+insert into object_on_map_details(id, hint, object_type, map_id) values ('14', 'Basen', 2, 1);
+
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('7', 50.0855076,19.9089341, 9);
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('8', 50.0854514,19.9111744, 10);
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('9', 50.0844259,19.9110967, 11);
+
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('10', 50.0855076,19.9089341, 12);
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('11', 50.0854514,19.9111744, 13);
+insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('12', 50.0844259,19.9110967, 14);
+
 
 insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('1', 255.0, 45.0, 4);
 insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('2', 85.0, 25.0, 3);
@@ -19,5 +36,5 @@ insert into lat_lng(id, latitude, longitude, object_on_map_details_id) values('6
 
 insert into my_user(id, username, password, email, is_active) values ('1', 'user1', 'password1', 'email1@email.pl', false);
 insert into my_user(id, username, password, email, is_active) values ('2', 'user2', 'password2', 'email2@email.pl', false);
-
+insert into my_user(id, username, password, email, is_active) values ('3', 'Monika', '123456', 'm@m.pl', false);
 
